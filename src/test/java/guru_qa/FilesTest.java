bc21ae147ecf9f776c363b73817939e603f059cf
iskander-d <drozenko21@gmail.com>
2024-11-24 12:10:04 +0300

    add check files in zip archive

--- src/test/java/guru_qa/FilesTest.java
+++ src/test/java/guru_qa/FilesTest.java
@@ -3,22 +3,73 @@ package guru_qa;
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
+import org.apache.commons.io.FilenameUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.List;
-
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class FilesTest {
     private ClassLoader cl = FilesTest.class.getClassLoader();
+
+    @DisplayName("Проверка на содержание необходимых файлов")
+    @Test
+    void verifyFilesInZip() throws Exception {
+
+        cl.getResourceAsStream("For qa_guru.zip");
+        {
+            {
+                ZipEntry entry;
+                List<String> expectedFiles = List.of("csv", "pdf", "xlsx");
+                List<String> expectedExtensions = List.of("csv", "pdf", "xlsx");
+
+
+                assertEquals(expectedFiles, expectedExtensions);
+            }
+        }
+    }
+    @DisplayName("Проверка на содержание необходимых файлов в архиве ")
+    @Test
+    void verifyFilesInZip2() throws IOException {
+        List<String> expectedExtensions = List.of("csv", "pdf", "xlsx");
+        Set<String> foundExtensions = new HashSet<>();
+
+        try (InputStream inputStream = cl.getResourceAsStream("For qa_guru.zip")) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("ZIP archive not found!");
+            }
+            try (ZipInputStream zis = new ZipInputStream(inputStream)) {
+                ZipEntry entry;
+                while ((entry = zis.getNextEntry()) != null) {
+                    if (!entry.isDirectory())
+                    {
+                        String fileName = entry.getName();
+                        String extension = FilenameUtils.getExtension(fileName);
+                        if (expectedExtensions.contains(extension)) {
+                            foundExtensions.add(extension);
+                        }
+                    }
+                }
+            }
+        }
+
+        assertTrue(foundExtensions.containsAll(expectedExtensions),
+                "Archive doesn't contain all expected files (csv, pdf, xlsx)");
+    }
 
     @DisplayName("Распаковка и проверка CVS из архива ZIP ")
     @Test
@@ -70,4 +121,5 @@ public class FilesTest {
             }
         }
     }
+
 }
